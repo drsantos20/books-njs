ef026a6c0e9dbf41d6f203789ec4883fff9936ee
Daniel Santos <drsantos20@gmail.com>
2016-05-15 19:53:00 -0300

    alteracoes de banco de dados

--- app/routes/db.js
+++ app/routes/db.js
@@ -1,17 +1,37 @@
-var knex = require('knex')({
-  client: 'mysql',
-  var url = process.env.CLEARDB_DATABASE_URL;
-  var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
-  connection: {
-	host:grupos[3],
-	user:grupos[1],
-	password:grupos[2],
-	database:grupos[4]
-  }
-});
+var mysql  = require('mysql');
 
-var bookshelf = require('bookshelf')(knex);
+function createDBConnection(){
+	if (!process.env.NODE_ENV || process.env.node === 'dev') {
+		return mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: 'root',
+			database: 'casadocodigo_nodejs'
+		});
+	}
 
-var User = bookshelf.Model.extend({
-  tableName: 'users'
-});
+	if (process.env.NODE_ENV == 'test') {
+		return mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '',
+			database: 'casadocodigo_nodejs_teste'
+		});
+	}
+
+	if (process.env.NODE_ENV == 'production') {
+		var url = process.env.CLEARDB_DATABASE_URL;
+		var grupos = url.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?/);
+		return mysql.createConnection({
+			host:grupos[3],
+			user:grupos[1],
+			password:grupos[2],
+			database:grupos[4]
+		});
+	}
+}
+
+
+module.exports = function() {
+	return createDBConnection;
+}
--- app/routes/model.js
+++ app/routes/model.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
   client: 'mysql',
   connection: {
-	   host: 'localhost',  // your host
-	   user: 'root', // your database user
-	   password: 'root', // your database password
-	   database: 'dbUsers',
+	   host: 'us-cdbr-iron-east-03.cleardb.net',  // your host
+	   user: 'b2b9ea565e5785', // your database user
+	   password: 'afe978b0', // your database password
+	   database: 'heroku_7abc9251d3ac94f',
 	   charset: 'UTF8_GENERAL_CI'
   }
 });
